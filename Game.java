45bf0f9285786d8d4796085138e3d4d6af88fd81
Matt Li <mli0@students.d125.org>
2018-05-10 10:43:33 -0500

    k

--- Game.java
+++ Game.java
@@ -193,34 +193,47 @@ public class Game
         if(orient.equals("right")){
             vert=false;
         }
-        else{
+        else if(orient.equals("up")){
             vert=true;
+        }
+        else{
+            System.out.println("Invalid input, defaulted to right");
+            vert=false;
         }
 
         Ship sub=new Submarine(xPos,yPos,vert);
+        int count=0;
         for(Coordinate c:sub.getCoordinates()){
-            while(c.getX()>9||c.getY()>9){
-                System.out.println("Invalid input, REEnter coordinates: ");
-                System.out.print("Location of Submarine: \nRow: ");
-                yPos=scan.nextInt();
-                System.out.print("\nCol: ");
-                xPos=scan.nextInt();
+            if(c.getX()>9||c.getY()>9){
+                count++;
+            }
+        }
+        while(count>0){
+            System.out.println("Invalid input, Reenter coordinates: ");
+            System.out.print("Location of Submarine: \nRow: ");
+            yPos=scan.nextInt();
+            System.out.print("\nCol: ");
+            xPos=scan.nextInt();
 
-                System.out.print("\nOrientation (right/up): ");
-                orient=scan.next();
+            System.out.print("\nOrientation (right/up): ");
+            orient=scan.next();
 
-                if(orient.equals("right")){
-                    vert=false;
+            if(orient.equals("right")){
+                vert=false;
+            }
+            else if(orient.equals("up")){
+                vert=true;
+            }
+            else{
+                System.out.println("Invalid input, defaulted to right");
+                vert=false;
+            }
+            count=0;
+            sub=new Submarine(xPos,yPos,vert);
+            for(Coordinate c:sub.getCoordinates()){
+                if(c.getX()>9||c.getY()>9){
+                    count++;
                 }
-                else if(orient.equals("up")){
-                    vert=true;
-                }
-                else{
-                    System.out.println("Invalid input, defaulted to right");
-                    vert=false;
-                }
-
-                sub=new Submarine(xPos,yPos,vert);
             }
         }
         return sub;
@@ -250,7 +263,42 @@ public class Game
         }
 
         Ship gun=new Gunboat(xPos,yPos,vert);
+        int count=0;
+        for(Coordinate c:gun.getCoordinates()){
+            if(c.getX()>9||c.getY()>9){
+                count++;
+            }
+        }
+        while(count>0){
+            System.out.println("Invalid input, Reenter coordinates: ");
+            System.out.print("Location of Gunboat: \nRow: ");
+            yPos=scan.nextInt();
+            System.out.print("\nCol: ");
+            xPos=scan.nextInt();
+
+            System.out.print("\nOrientation (right/up): ");
+            orient=scan.next();
+
+            if(orient.equals("right")){
+                vert=false;
+            }
+            else if(orient.equals("up")){
+                vert=true;
+            }
+            else{
+                System.out.println("Invalid input, defaulted to right");
+                vert=false;
+            }
+            count=0;
+            gun=new Gunboat(xPos,yPos,vert);
+            for(Coordinate c:gun.getCoordinates()){
+                if(c.getX()>9||c.getY()>9){
+                    count++;
+                }
+            }
+        }
         return gun;
+
 
     }
 
