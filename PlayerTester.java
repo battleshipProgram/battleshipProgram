0d4dd1c4e0e838130bb328964acf44f6bb333fd8
Anand Vadlamani <mli0@students.d125.org>
2018-05-10 10:50:10 -0500

    h

--- PlayerTester.java
+++ PlayerTester.java
@@ -1,38 +1,44 @@
-
-/**
- * PlayerTester.java  
- *
-<<<<<<< HEAD
- * @author:matt li
-=======
- * @author: Matt Li
->>>>>>> 87f77e63ca11ca9d88032de70ab5510cc4a80583
- * Assignment #:
- * 
- * Brief Program Description: This is the tester for the Player class.
- * 
- *
- */
-public class PlayerTester
-{
-    public static void main (String [] args)
-    {
-        Player player = new Player(Submarine s, Gunboat g, Carrier c, Battleship b, Board xd);
+// import java.util.ArrayList; 
+// /**
+ // * PlayerTester.java  
+ // *
+// <<<<<<< HEAD
+ // * @author:matt li
+// =======
+ // * @author: Matt Li
+// >>>>>>> 87f77e63ca11ca9d88032de70ab5510cc4a80583
+ // * Assignment #:
+ // * 
+ // * Brief Program Description: This is the tester for the Player class.
+ // * 
+ // *
+ // */
+// public class PlayerTester
+// {
+    // public static void main (String [] args)
+    // {
+        // Ship s = new Submarine(5,5,true); 
+        // Ship g = new Gunboat(1,1,true);
+        // Ship c = new Carrier(8,8,false);
+        // Ship b = new Battleship(4,4,false);  
+        // Board xd = new Board(); 
+        // Player player = new Player(s,  g, c, b, xd);
         
-        // tests the guess method. will return true if the player's guess hits a coordinate
-        // in the opponent's array list of ships
-        player.guess(5,5,ArrayList <Ship> l);
+        // // tests the guess method. will return true if the player's guess hits a coordinate
+        // // in the opponent's array list of ships
+        // l = 
+        // player.guess(5,5,l);
         
-        // if the player misses, the location is marked so the player knows not to guess there
-        // again
-        player.markGuess();
+        // // if the player misses, the location is marked so the player knows not to guess there
+        // // again
+        // player.markGuess();
         
-        // if the player hits an opponent's ships, the location is marked. if an opponent's ship
-        // is sunk, the opponent's ship will be removed from its array list
-        player.markHit();
+        // // if the player hits an opponent's ships, the location is marked. if an opponent's ship
+        // // is sunk, the opponent's ship will be removed from its array list
+        // player.markHit();
         
-        // if a player's ship is hit, the coordinate of the ship will be removed. If a player's
-        // ship is sunk, the ship will be removed from the array list
-        player.playerHit();
-    }
-}
+        // // if a player's ship is hit, the coordinate of the ship will be removed. If a player's
+        // // ship is sunk, the ship will be removed from the array list
+        // player.playerHit();
+    // }
+// }
