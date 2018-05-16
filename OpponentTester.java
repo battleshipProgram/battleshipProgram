b7955e8ca4a393d3cbb85bdb51df6c4580f3cf59
Anand Vadlamani <mli0@students.d125.org>
2018-05-16 10:58:21 -0500

    hkg,

--- OpponentTester.java
+++ OpponentTester.java
@@ -13,13 +13,16 @@ public class OpponentTester
 {
     public static void main (String [] args)
     {
-        //creates the Ship objects necessary to be used in the Opponent object
         
-        
-        //creates the Opponent object and Player object
+        //creates the Opponent and Board objects
         Board xd = new Board();
         Opponent opponent = new Opponent();
+        
+        //tests the setShips() method, should put the opponent's ships in random locations, making sure
+        //to keep the ships within the boundaries of the board and not overlapping each other
         opponent.setShips();
+        
+        //lets the player hit the opponent's ships, making each spot on the board be marked with an X
         for(Ship s: opponent.getOpponentShips())
         {
             for(Coordinate c: s.getCoordinates())
@@ -27,6 +30,9 @@ public class OpponentTester
                 xd.playerHit(c.getX(), c.getY());
             }
         }
+        
+        //prints the opponent's board with ships on it, should all be X because of the nested for loops
+        //above
         xd.printBoard();
         
     }
